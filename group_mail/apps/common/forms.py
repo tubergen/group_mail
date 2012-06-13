26868475ff0d766ac922255380f65a460a3bb3c1
Brian Tubergen <brian.tubergen@gmail.com>
2012-06-13 01:12:05 +0000

    fixed a major flaw in email claiming where claiming non-primary emails was broken. however, note that any claiming relating to new accounts is still broken.

--- group_mail/apps/common/forms.py
+++ group_mail/apps/common/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import PasswordResetForm
+# from django.contrib.auth.forms import PasswordResetForm
 from group_mail.apps.common.models import CustomUser
-from group_mail.apps.group.forms import UserEmailForm
+from group_mail.apps.registration.forms import UserEmailForm
 
 
 class AddEmailForm(UserEmailForm):
@@ -28,7 +28,7 @@ class AddEmailForm(UserEmailForm):
                 raise CustomUser.DuplicateEmail(email=email, include_claim_link=True)
 
 
-class ClaimEmailForm(PasswordResetForm):
+class ClaimEmailForm(forms.Form):
     email = forms.EmailField(max_length=CustomUser.MAX_LEN)
 
     def __init__(self, claim_user, *args, **kwargs):
@@ -36,7 +36,7 @@ class ClaimEmailForm(PasswordResetForm):
         self.claim_user = claim_user
 
     def clean_email(self):
-        email = super(ClaimEmailForm, self).clean_email()
+        email = self.cleaned_data['email']
         if self.claim_user.has_email(email):
             raise CustomUser.DuplicateEmail(msg='The email already belongs to your account.')
         return email
@@ -57,25 +57,24 @@ class ClaimEmailForm(PasswordResetForm):
         from django.utils.http import int_to_base36
         from django.core.mail import send_mail
 
-        # PasswordResetForm populates users_cache in clean_email()
-        for user in self.users_cache:
-            current_site = get_current_site(request)
-            site_name = current_site.name
-            domain = current_site.domain
-            # below, we use claim_user to generate the uid and token.
-            # this is because, if the 'claim request' is successful, claim_user
-            # is the account to which we'll add the email
-            c = {
-                'email': user.email,
-                'domain': domain,
-                'site_name': site_name,
-                'uid': int_to_base36(claim_user.id),
-                'claim_user': claim_user,
-                'token': default_token_generator.make_token(claim_user),
-                'protocol': 'http',
-            }
-            subject = loader.render_to_string(subject_template_name, c)
-            # Email subject *must not* contain newlines
-            subject = ''.join(subject.splitlines())
-            email = loader.render_to_string(email_template_name, c)
-            send_mail(subject, email, from_email, [user.email])
+        current_site = get_current_site(request)
+        site_name = current_site.name
+        domain = current_site.domain
+        claim_email_addr = self.cleaned_data['email']
+        # below, we use claim_user to generate the uid and token.
+        # this is because, if the 'claim request' is successful, claim_user
+        # is the account to which we'll add the email
+        c = {
+            'email': claim_email_addr,
+            'domain': domain,
+            'site_name': site_name,
+            'uid': int_to_base36(claim_user.id),
+            'claim_user': claim_user,
+            'token': default_token_generator.make_token(claim_user),
+            'protocol': 'http',
+        }
+        subject = loader.render_to_string(subject_template_name, c)
+        # Email subject *must not* contain newlines
+        subject = ''.join(subject.splitlines())
+        email = loader.render_to_string(email_template_name, c)
+        send_mail(subject, email, from_email, [claim_email_addr])
